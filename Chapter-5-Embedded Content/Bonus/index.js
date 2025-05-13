59d0eea6e2cf0007f06dd7fc7ee609ef3190c3a3
Aryan.Udayakumar <aryanbsu132@gmail.com>
2025-05-13 12:54:11 +0400

    Delete Chapter-5-Embedded Content directory

--- Chapter-5-Embedded Content/Bonus/index.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.sample-button').forEach(button => {
-    button.addEventListener('click', () => {
-      const audio = new Audio(button.getAttribute('data-audio'));
-      audio.play();
-    });
-  });
-  
